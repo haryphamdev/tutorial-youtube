e07573a035c69e2daf9f27f3a1cf3aaf68efc101
Pham Tuan <jvgiveup@gmail.com>
2020-05-12 17:14:05 +0700

    config webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from "express";
 import homeController from "./../controllers/homeController";
+import chatFacebookController from "./../controllers/chatFacebookController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeController.handleWebHelloWorld);
+    router.post("/webhook", chatFacebookController.postWebHook);
+    router.get("/webhook", chatFacebookController.getWebHook);
     return app.use("/", router);
 };
 module.exports = initWebRoutes;
